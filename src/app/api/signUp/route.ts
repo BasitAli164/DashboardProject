67240920f6b864640791f675e1b5ae7179e47979
Basit Ali <opoali911@gmail.com>
2025-03-07 21:45:24 +0500

    exploring new thing in next js

--- src/app/api/signUp/route.ts
+++ src/app/api/signUp/route.ts
@@ -3,23 +3,40 @@ import userModel from "@/model/User.model";
 import bcrypt from "bcryptjs";
 import { sendVarificationEmail } from "@/helpers/sendVerificationEmail";
 
-export const POST=async(request:Request)=>{
-    await dbConnect();
-    try {
-      const {username,email,password} = await request.json();
-        
-    } catch (error) {
-        console.error("Error registering user",error)
-        return Response.json({
-            success:false,
-            message:"Error registering user"
+export const POST = async (request: Request) => {
+  await dbConnect();
+  try {
+    const { username, email, password } = await request.json();
+    const existingUserVerifiedByUsername = await userModel.findOne({
+      username,
+      isVerified: true,
+    });
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        {
+          success: false,
+          message: "Username is already taken",
         },
-    {
-        status:5000,
-        statusText:"Server site Error in SignUp Route"
+        {
+          status: 400,
+          statusText: "Client Site Problem",
+        }
+      );
     }
-    )
-        
-    }
-
-}
+    const existingUserVerifiedByEmail = await userModel.findOne({
+      email,
+    });
+  } catch (error) {
+    console.error("Error registering user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error registering user",
+      },
+      {
+        status: 5000,
+        statusText: "Server site Error in SignUp Route",
+      }
+    );
+  }
+};
