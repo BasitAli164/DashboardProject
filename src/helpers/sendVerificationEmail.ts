e1cebc3137b49ab6eb21ecf9554d5df54471dc11
Basit Ali <opoali911@gmail.com>
2025-03-07 21:27:22 +0500

    create the email process

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,27 @@
 import { resend } from "@/lib/resend";
 import {VerificationEmail} from '../../emails/VerificationEmail'
+import { ApiResponse } from "@/types/ApiResponse";
+
+const sendVarificationEmail=async(
+    email:string,
+    userName:string,
+    verifyCode:string,
+):Promise<ApiResponse>=>{
+    try {
+        await resend.emails.send({
+            from:"NetBots Company",
+            to:email,
+            subject:"NetBots Company |Verification Code!",
+            react:VerificationEmail({userName,otp:verifyCode})
+            
+        })
+        return {success:true,message:"Verification email send successfully "}
+        
+    } catch (emailError) {
+        console.error("Error sending verification email",emailError)
+        return {success:false,message:"Fail to send verification email."}
+        
+    }
+
+
+}
