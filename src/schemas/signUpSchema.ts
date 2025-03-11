041413f2033e449255004523364aed39c76b3d9d
Basit Ali <opoali911@gmail.com>
2025-03-12 00:05:26 +0500

    making new api

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -1,5 +1,11 @@
 import { z } from "zod";
 
+export const usernameValidation= z
+.string()
+.min(2, "Username must be at least 2 characters")
+.max(20, "Username must be no more  than 20 characters ")
+.regex(/^[a-zA-Z0-9]+$/, "Username must not contain special character")
+
 export const signUpValidation = z.object({
   username: z
     .string()
