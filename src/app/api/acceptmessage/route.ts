69078f16a0d51abe6b3a5c592e4921bcff35a98c
Basit Ali <opoali911@gmail.com>
2025-03-12 01:13:08 +0500

    working in the message route

--- src/app/api/acceptmessage/route.ts
+++ src/app/api/acceptmessage/route.ts
@@ -3,3 +3,45 @@ import { authOption } from "../auth/[...nextauth]/option";
 import dbConnect from "@/lib/dbConnect";
 import userModel from "@/model/User.model";
 import { User } from "next-auth";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  const session = await getServerSession(authOption);
+  const user: User = session?.user as User;
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "Not Authenticated",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+  const userId = user._id;
+  const { acceptMessages } = await request.json();
+
+  try {
+    await userModel.findByIdAndUpdate(
+      userId,
+      {
+        isAcceptingMessage: acceptMessages,
+      },
+      {
+        new: true,
+      }
+    );
+  } catch (error) {
+    console.error("Failed to update user status to accept messages");
+    return Response.json(
+      {
+        success: false,
+        message: "Failed to update user status to accept messages",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
