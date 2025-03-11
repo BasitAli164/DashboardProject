9ac173d28474061e4757f8e5a2eaed9a4fc35214
Basit Ali <opoali911@gmail.com>
2025-03-12 00:18:18 +0500

    working in the checking the username

--- src/app/api/checkunique/route.ts
+++ src/app/api/checkunique/route.ts
@@ -2,3 +2,32 @@ import dbConnect from "@/lib/dbConnect";
 import userModel from "@/model/User.model";
 import z from 'zod'
 import { usernameValidation } from "@/schemas/signUpSchema";
+
+const UsernameQuerySchema=z.object({
+    username:usernameValidation
+})
+
+export async function GET(request:Request){
+    await dbConnect();
+    try {
+        const {searchParams}=new URL(request.url) // extract the query from url
+        const queryParam={
+            username:searchParams.get('username')
+        }
+        const result=UsernameQuerySchema.safeParse(queryParam)
+        
+        
+    } catch (error) {
+    console.error("Error Checking username",error)   
+    return Response.json(
+        {
+            success:false,
+            message:"Error Checking userName"
+        },
+        {
+            status:500
+        }
+    )     
+    }
+
+}
