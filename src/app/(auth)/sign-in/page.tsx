27c8b9991f9849073b0c2aff14896953262d6eb1
Basit Ali <opoali911@gmail.com>
2025-03-12 23:52:42 +0500

    fix the error

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
+export default function SignInPage() {
+  const { data: session } = useSession();
+  if(session){
+   return (
+    <>
+    Signed in as {session.user.username} <br />
 
-const SignInPage = () => {
+    <button onClick={()=>signOut()}>SignOut</button>
+    </>
+   ) 
+  }
   return (
-    <div>
-      page
-    </div>
+    <>
+    Not-Sign In <br />
+    <button className="bg-orange-500 px-3 py-1 m-4 rounded" onClick={()=>signIn()}>Sign In</button>
+    </>
   )
 }
-
-export default SignInPage
