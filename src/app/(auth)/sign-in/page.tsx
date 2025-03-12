46ef59c6b44bf16ba961bc34dd23b136a3be582d
Basit Ali <opoali911@gmail.com>
2025-03-12 23:34:45 +0500

    fixing errors

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -5,9 +5,16 @@ export default function SignInPage() {
   if(session){
    return (
     <>
-    Signed in as {session.user.username}
-    <button></button>
+    Signed in as {session.user.username} <br />
+
+    <button onClick={()=>signOut()}>SignOut</button>
     </>
    ) 
   }
+  return (
+    <>
+    Not-Sign In <br />
+    <button className="bg-orange-500 px-3 py-1 m-4 rounded" onClick={()=>signIn()}>Sign In</button>
+    </>
+  )
 }
