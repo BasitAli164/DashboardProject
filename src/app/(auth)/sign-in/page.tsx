087bda790688b313f665d7475bbf5ceba461077c
Basit Ali <opoali911@gmail.com>
2025-03-12 23:22:38 +0500

    start to make the context

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,5 +2,12 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 export default function SignInPage() {
   const { data: session } = useSession();
-  
+  if(session){
+   return (
+    <>
+    Signed in as {session.user.username}
+    <button></button>
+    </>
+   ) 
+  }
 }
